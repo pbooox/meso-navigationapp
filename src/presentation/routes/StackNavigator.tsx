6c0ea91a55c47ad128c2f96e93c36dcaa1b5dbe5
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-10-10 11:57:04 -0600

    hasta stack navigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -2,15 +2,31 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { ProductsScreen } from '../screens/products/ProductsScreen';
 import { SettingsScreen } from '../screens/stettings/SettingsScreen';
+import { ProductScreen } from '../screens/products/ProductScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+  Home: undefined,
+  Product: { id: number, name: string },
+  Products: undefined,
+  Settings: undefined
+}
 
-export const  StackNavigator = () =>  {
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Products" component={ProductsScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          elevation: 0
+        }
+      }}
+    >
+      <Stack.Screen name="Home" component={ HomeScreen } />
+      <Stack.Screen name="Products" component={ ProductsScreen } />
+      <Stack.Screen name="Product" component={ ProductScreen } />
+      <Stack.Screen name="Settings" component={ SettingsScreen } />
     </Stack.Navigator>
   );
 }
