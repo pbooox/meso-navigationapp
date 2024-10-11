f7c54110a5382d62da76a5e1c7a1a383e2362c74
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-10-11 06:59:06 -0600

    terminando drawer navigator

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,23 +1,38 @@
-import { View } from "react-native"
+import { Text, View } from "react-native"
 import { globalStyles } from "../../theme/theme"
-import { NavigationProp, useNavigation } from "@react-navigation/native"
+import { DrawerActions, NavigationProp, useNavigation } from "@react-navigation/native"
 import { PrimaryButton } from "../../components/shared/PrimaryButton";
 import type { RootStackParams } from "../../routes/StackNavigator";
+import { useEffect } from "react";
+import { Pressable } from "react-native-gesture-handler";
 
 export const HomeScreen = () => {
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer ) }
+        >
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    })
+  }, [])
+  
+
   return (
     <View style={globalStyles.container} >
-        <PrimaryButton
-          onPress={ () => navigation.navigate('Products') }
-          label="Productos"
-        />
-        <PrimaryButton
-          onPress={ () => navigation.navigate('Settings') }
-          label="Settings"
-        />
+      <PrimaryButton
+        onPress={ () => navigation.navigate('Products') }
+        label="Productos"
+      />
+      <PrimaryButton
+        onPress={ () => navigation.navigate('Settings') }
+        label="Settings"
+      />
     </View>
   )
 }
